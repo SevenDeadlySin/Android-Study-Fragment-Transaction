03c699ebaafb936983d66345c6bdf545852a4d9d
BamBo <raksameth@gmail.com>
2017-04-02 17:04:45 +0700

    Add events to the buttons

--- Android Fragment Transaction/app/src/main/java/com/raksa/androidfragmenttransaction/MainActivity.java
+++ Android Fragment Transaction/app/src/main/java/com/raksa/androidfragmenttransaction/MainActivity.java
@@ -2,6 +2,8 @@ package com.raksa.androidfragmenttransaction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonAddA = (Button) findViewById(R.id.buttonAdd1);
+        Button buttonRemA = (Button) findViewById(R.id.buttonRem1);
+        Button buttonAddB = (Button) findViewById(R.id.buttonAdd2);
+        Button buttonRemB = (Button) findViewById(R.id.buttonRem2);
+        
+
+        buttonAddA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonRemA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        buttonAddB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonRemB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
